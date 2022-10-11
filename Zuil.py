16f202dcfe08e17ca2ac08ed6537948cf84bfff2
Frederiquegroot <grootfrederique@gmail.com>
2022-10-11 10:41:44 +0200

    Zuil

--- Zuil.py
+++ Zuil.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+import random
 naam = input('wat is je naam?')
+if naam == '':
+    naam = 'Anoniem'
+
 mening = input('wat is jou mening?')
 
 now = datetime.now()
 data = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+Stations = ('Zwolle','Sittard','Oss')
 
 outfile = open ('Mening.txt', 'a')
 outfile.write(naam)
