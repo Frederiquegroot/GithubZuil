5f5cb5e78cb53a521d0655e6becac3e210db3ba8
Frederiquegroot <grootfrederique@gmail.com>
2022-10-13 09:05:22 +0200

    Zuil

--- Zuil.py
+++ Zuil.py
@@ -1,10 +1,5 @@
 from datetime import datetime
 import random
-naam = input('wat is je naam?')
-if naam == '':
-    naam = 'Anoniem'
-
-mening = input('wat is jou mening?')
 
 now = datetime.now()
 data = now.strftime("%m/%d/%Y,%H:%M:%S")
@@ -12,6 +7,18 @@ data = now.strftime("%m/%d/%Y,%H:%M:%S")
 stations = ('Zwolle','Sittard','Oss')
 station = random.choice(stations)
 
-outfile = open ('Mening.txt', 'a')
-outfile.write(naam+','+mening+','+data+','+station)
-print(naam+','+mening+','+data+','+station)
+naam = input('wat is je naam?')
+if naam == '':
+    naam = 'Anoniem'
+
+mening = input('wat is jou mening?')
+
+if len(mening) > 140:
+    print('je mening is te lang!')
+else:
+    outfile = open ('Mening.txt', 'a')
+    outfile.write('\n'+naam+','+mening+','+data+','+station)
+    print(naam+','+mening+','+data+','+station)
+
+
+
