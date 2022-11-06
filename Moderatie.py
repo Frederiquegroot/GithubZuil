c6eb724a89b3665d2a160acc1e60db0b48295693
Frederiquegroot <grootfrederique@gmail.com>
2022-11-06 13:58:45 +0100

    Zuil

--- Moderatie.py
+++ Moderatie.py
@@ -2,15 +2,15 @@ from datetime import datetime
 import random
 import psycopg2
 from tkinter import *
-def psycopg2(naamMening,bericht,datum,tijd,station_stad,tijdBeoordeling,datumBeoordeling,naamModerator):
+def psycopg(naamMening,bericht,datum,tijd,station_stad,tijdBeoordeling,datumBeoordeling,naamModerator):
 
 
-    connection_string= "host='localhost' dbname='zuil FA' user='postgres' password='Molly123fred!'"
+    connection_string= "host='localhost' dbname='zuilFA' user='postgres' password='Molly123fred!'"
     conn = psycopg2.connect(connection_string)
     cursor = conn.cursor()
 
-    insert="""INSERT INTO bericht values(beoordelingID,naamMening,tijd,datum,tijdBeoordeling,DatumBeoordeling,station_stad,naamModerator,bericht)
-            values(DEFAULT,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    insert="""INSERT INTO bericht(beoordelingID,naamMening,tijd,datum,tijdBeoordeling,DatumBeoordeling,station_stad,naammoderator,bericht)
+            VALUES(DEFAULT,%s,%s,%s,%s,%s,%s,%s,%s)"""
 
 
     data = (naamMening,tijd,datum,tijdBeoordeling,datumBeoordeling,station_stad,naamModerator,bericht)
@@ -36,18 +36,19 @@ def moderatie():
         beoordeling = input('typ y voor goedgekeurd en n voor foutgekeurd: ')
 
 
-    if beoordeling == 'y':
-        psycopg2(berichtmening[0], berichtmening[1], berichtmening[2], berichtmening[3], berichtmening[4],
+        if beoordeling == 'y':
+            psycopg(berichtmening[0], berichtmening[1], berichtmening[2], berichtmening[3], berichtmening[4],
                  tijdbeoordeling, datumbeoordeling, moderator)
 
 
-    elif beoordeling =='n':
-       outfile = open('Mening.txt', 'a')
-       outfile.write(moderator + ';' + emailmoderator + ';' + databeoordeling + ';' + beoordeling + ';' + '\n')
-       print(moderator + ';' + emailmoderator + ';' + databeoordeling + ';' + beoordeling + ';')
+        elif beoordeling =='n':
 
-    else:
-        print('typ alleen y of n in')
+            outfile = open('Mening.txt', 'a')
+            outfile.write(moderator + ';' + emailmoderator + ';' + databeoordeling + ';' + beoordeling + ';' + '\n')
+            print(moderator + ';' + emailmoderator + ';' + databeoordeling + ';' + beoordeling + ';')
+
+        else:
+            print('typ alleen y of n in')
 
     outfile.close()
     #outfile= open('mening.txt','w')
